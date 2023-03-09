35823307140db86d00310e3c57dfc7df325b35eb
ahmadlogs <73652235+ahmadlogs@users.noreply.github.com>
2023-03-09 08:47:54 +0500

    Add files via upload

---
+++ rfid-attendance-v4.ino
@@ -0,0 +1,19 @@
+/* ------------------------------------------------------------------------
+ * Created by: Tauseef Ahmad
+ * Created on: 17 July, 2022
+ *  
+ * Tutorial: https://youtu.be/Bgs_3F5rL5Q
+ * ------------------------------------------------------------------------
+ * Download Resources
+ * ------------------------------------------------------------------------
+ * Preferences--> Aditional boards Manager URLs : 
+ * For ESP8266 and NodeMCU - Board Version 2.6.3
+ * http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ * ------------------------------------------------------------------------
+ * HTTPS Redirect Library:
+ * https://github.com/jbuszkie/HTTPSRedirect
+ * Example Arduino/ESP8266 code to upload data to Google Sheets
+ * Follow setup instructions found here:
+ * https://github.com/StorageB/Google-Sheets-Logging
+ * ------------------------------------------------------------------------*/
+ 
